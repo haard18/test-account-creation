83b61f85d921bd3b10bf615879b66c3799fe1592
Haard Solanki <haardsolanki.itm@gmail.com>
2024-06-11 11:52:49 +0530

    Asset Creation

---
+++ issueasset.js
@@ -0,0 +1,66 @@
+var StellarSdk = require("stellar-sdk");
+var server = new StellarSdk.Horizon.Server(
+  "https://horizon-testnet.stellar.org",
+);
+
+// Keys for accounts to issue and receive the new asset
+var issuingKeys = StellarSdk.Keypair.fromSecret(
+  "SD5GY5SHX6XZKNDQRP66GKQPWTLP3ODVDDFVLDUG7SBXB3IXLVYDPBED",
+);
+var receivingKeys = StellarSdk.Keypair.fromSecret(
+  "SDDGSCXFAGDGQXG2QEUMJCD24TIL3ETUMCHUFUKYVYV5DQ4ZNW5JLMMH",
+);
+
+// Create an object to represent the new asset
+var NewDollar = new StellarSdk.Asset("NewDollar", issuingKeys.publicKey());
+
+// First, the receiving account must trust the asset
+server
+  .loadAccount(receivingKeys.publicKey())
+  .then(function (receiver) {
+    var transaction = new StellarSdk.TransactionBuilder(receiver, {
+      fee: 100,
+      networkPassphrase: StellarSdk.Networks.TESTNET,
+    })
+      // The `changeTrust` operation creates (or alters) a trustline
+      // The `limit` parameter below is optional
+      .addOperation(
+        StellarSdk.Operation.changeTrust({
+          asset: NewDollar,
+          limit: "1000",
+        }),
+      )
+      // setTimeout is required for a transaction
+      .setTimeout(100)
+      .build();
+    transaction.sign(receivingKeys);
+    return server.submitTransaction(transaction);
+  })
+  .then(console.log)
+
+  // Second, the issuing account actually sends a payment using the asset
+  .then(function () {
+    return server.loadAccount(issuingKeys.publicKey());
+  })
+  .then(function (issuer) {
+    var transaction = new StellarSdk.TransactionBuilder(issuer, {
+      fee: 100,
+      networkPassphrase: StellarSdk.Networks.TESTNET,
+    })
+      .addOperation(
+        StellarSdk.Operation.payment({
+          destination: receivingKeys.publicKey(),
+          asset: NewDollar,
+          amount: "10",
+        }),
+      )
+      // setTimeout is required for a transaction
+      .setTimeout(100)
+      .build();
+    transaction.sign(issuingKeys);
+    return server.submitTransaction(transaction);
+  })
+  .then(console.log)
+  .catch(function (error) {
+    console.error("Error!", error);
+  });
